2a366f4d4ad502ba57a33816f823984ad8ac1386
roberto villablanca <roberto.villablanca.1996@gmail.com>
2023-07-01 02:14:49 -0400

    js casi listo

--- assets/variableglobal.js
+++ assets/variableglobal.js
@@ -1,11 +1,41 @@
-let divazul = document.querySelector("#azul");
+let divazul = document.getElementById("azul");
 let divrojo = document.querySelector("#rojo");
 let divverde = document.querySelector("#verde");
 let divamarillo = document.querySelector("#amarillo");
-let divkey = docimen.querySelector("#key");
+let divkey = document.querySelector("#key");
 
 let changecolor = (color) => {
-  color.target.style.color = "black";
+  color.target.style.backgroundColor = "black";
 };
 
 divazul.addEventListener("click", changecolor);
+divrojo.addEventListener("click", changecolor);
+divverde.addEventListener("click", changecolor);
+divamarillo.addEventListener("click", changecolor);
+
+function creatediv(color) {
+  let diiv = document.createElement("div");
+  diiv.style.width = "200px";
+  diiv.style.height = "200px";
+
+  let nuevotex = document.createTextNode("hi");
+  diiv.appendChild(nuevotex);
+  diiv.style.backgroundColor = color;
+  document.body.appendChild(diiv);
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "a") {
+    azul.style.backgroundColor = "pink";
+  } else if (event.key === "s") {
+    divrojo.style.backgroundColor = "orange";
+  } else if (event.key === "d") {
+    divverde.style.backgroundColor = "aqua";
+  } else if (event.key === "q") {
+    creatediv("skyblue");
+  } else if (event.key === "w") {
+    creatediv("orange");
+  } else if (event.key === "e") {
+    creatediv("brown");
+  }
+});
